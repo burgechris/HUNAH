2f44fd97ff6acb13007f0d92017211b537627f68
Chris Burge and sofia giordano <chris@>
2019-10-15 10:34:08 -0700

    add index, detail, and create view content.

--- Hunah/Models/Animal.cs
+++ Hunah/Models/Animal.cs
@@ -2,7 +2,10 @@ namespace Hunah.Models
 {
     public class Animal
     {
+        // animal object properties
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Breed { get; set; }
         public int AnimalId { get; set; }
-        public string Description { get; set; }
     }
 }
